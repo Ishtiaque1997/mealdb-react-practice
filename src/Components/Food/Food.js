32ba0c478333a3e098ad9f99d0eb90b795cd6ed8
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-16 11:20:41 +0600

    all-done

--- src/Components/Food/Food.js
+++ src/Components/Food/Food.js
@@ -1,8 +1,15 @@
 import Button from '@restart/ui/esm/Button';
 import React from 'react';
-import { Card } from 'react-bootstrap';
-import './Food.css'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
+import { Card } from 'react-bootstrap';
+import './Food.css';
+
+// const element=(class) =>{
+//    class='fas fa-coffee'
+// }
+// const element = <FontAwesomeIcon icon={faCoffee} />
 const Food = (props) => {
  const{strMeal,strCategory,strArea,strMealThumb}=props.food;
  return (
@@ -18,11 +25,12 @@ const Food = (props) => {
      <br />
      Region:{strArea}
     </Card.Text>
-    <Button onClick={()=>props.handleOrder(props.food)} variant="primary"bg="secondary">Order food</Button>
+    <Button onClick={()=>props.handleOrder(props.food)} variant="primary"bg="secondary"><i class="fas fa-coffee"></i>Order food</Button>
   </Card.Body>
 </Card>
-  
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"></link>
   </div>
+  
  );
 };
 
